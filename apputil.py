b1bfe69f83bb3fcc1ba1513f5d1db6ee936fc1e5
Woods Procise <wprocise@iu.edu>
2025-11-05 18:35:58 -0500

    new updates

--- apputil.py
+++ apputil.py
@@ -4,8 +4,16 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.cluster import KMeans
 
+"""Define kmeans and perform clustering on numerical numpy array"""
 def kmeans(X, k):
 
     model = KMeans(n_clusters=k, random_state=42)
     model.fit(X)
+    
+    centroids = model.cluster_centers_
+    labels = model.labels_
+
+    return (centroids, labels)
+
+
 
