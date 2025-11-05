4ca377658b4b9e5860c59b825d01bea7de7171d7
Woods Procise <wprocise@iu.edu>
2025-11-05 18:31:01 -0500

    defining kmeans

--- apputil.py
+++ apputil.py
@@ -2,4 +2,10 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.cluster import KMeans
+
+def kmeans(X, k):
+
+    model = KMeans(n_clusters=k, random_state=42)
+    model.fit(X)
 
